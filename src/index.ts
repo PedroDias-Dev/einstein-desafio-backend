16f76ba14244d838132e677e1763b12f6a3cc265
Pedro Dias <pedrofelipe18@hotmail.com.br>
2024-11-22 20:52:54 -0300

    chore: implement survey router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
+import SurveyRouter from "./routes/surveys";
+
 dotenv.config();
 
 const app: Express = express();
@@ -12,6 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
   res.send("The server is running");
 });
+
+app.use("/api/v1/survey", SurveyRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
